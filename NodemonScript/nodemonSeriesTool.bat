143ce4378e24db71e35fb25b74dc92152f757df1
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-11 05:40:26 +0100

    Update nodemonSeriesTool.bat.

--- NodemonScript/nodemonSeriesTool.bat
+++ NodemonScript/nodemonSeriesTool.bat
@@ -4,20 +4,28 @@ cls
 
 REM setting variables
 set scriptVersion=1.0
-set logFile=%~dp0%~n0.log
-
+set logFile="%~dp0%~n0.log"
+set errFile="%~dp0%~n0.err"
+set nodemon="c:\Users\Administrator\AppData\Roaming\npm\nodemon.cmd"
 Echo.
 Echo.
 Echo Starting SeriesTool with nodemon...
 Echo.
 Echo Using log file: %logFile%
+Echo Using error file: %errFile%
 Echo.
 Echo.  >> %logFile%
 Echo %date% - %time% >> %logFile%
+Echo.  >> %errFile%
+Echo %date% - %time% >> %errFile%
+echo path: %path% >> %logFile%
+echo nodemon.cmd: %nodemon% >> %logFile%
+
 Echo Changeing directory...
-cd s:\documenten\github\SeriesTool >> %logFile%
+z: 
+cd \_Shares\wwwroot\nodejs\seriestool >> %logFile% 2>> %errFile% 
 Echo.
 Echo current directory: %cd% >> %logFile%
 Echo starting nodemon...
-start /wait "Nodemom" /min /REALTIME "nodemon" >> %logFile% >> %logFile%
+start /wait "Nodemon" /min /REALTIME %nodemon% >> %logFile% 2>> %errFile%
 REM start /wait "Nodemom" "nodemon" >> %logFile% >> %logFile%
